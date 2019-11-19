a9354dd7d9d3eb4e95c8dff480715acf585299a4
adeltancsik <tancsikadel@gmail.com>
2019-11-19 16:58:19 +0100

    Joining a room

--- room/model.js
+++ room/model.js
@@ -1,11 +1,20 @@
 const Sequelize = require("sequelize");
 const sequelize = require("../db");
+const User = require("../user/model");
 
-const Room = sequelize.define("room", {
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
+const Room = sequelize.define(
+  "room",
+  {
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+  },
+  {
+    timestamps: false
   }
-});
+);
+
+User.belongsTo(Room);
 
 module.exports = Room;
