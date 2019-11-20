d1a120face3bade8ddaa7e81fd3af5741087c983
adeltancsik <tancsikadel@gmail.com>
2019-11-20 16:56:59 +0100

    Create point endpoint and point column to User

--- quiz/router.js
+++ quiz/router.js
@@ -2,11 +2,35 @@ const { Router } = require("express");
 const router = new Router();
 const Questions = require("./questionModel");
 const Answers = require("./answerModel");
+const User = require("../user/model");
 
 router.get("/questions", (req, res, next) =>
   Questions.findAll({ include: [Answers] })
     .then(result => res.json(result))
     .catch(err => next(err))
 );
+
+router.put("/points/:userId", async (req, res, next) => {
+  const { userId } = req.params;
+
+  const user = await User.findByPk(userId);
+
+  const updated = await user.update({ points: 1 });
+
+  // const rooms = await Room
+  //   .findAll({ include: [User] })
+
+  // const action = {
+  //   type: 'ROOMS',
+  //   payload: rooms
+  // }
+
+  // const string = JSON
+  //   .stringify(action)
+
+  // stream.send(string)
+  console.log("updated testinggg:", updated);
+  res.send(updated);
+});
 
 module.exports = router;
